2915ff08e30f05590b11110f58c2f81ba0439198
timmy <timothy.ndung'u@student.moringaschool.com>
2022-05-19 09:18:47 +0300

    Procfile

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Config(object):
     '''
     General configuration class
     '''
-    # # RANDOM_QOUTES_URL = os.environ.get('QUOTES_URL')
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'wtf'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-    # 'postgres://', 'postgresql://') or\
-    # 'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False #configuration to disable auto updates to database
+    # RANDOM_QOUTES_URL = os.environ.get('QUOTES_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'wtf'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+    'postgres://', 'postgresql://') or\
+    'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False #configuration to disable auto updates to database
 
-    # LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
